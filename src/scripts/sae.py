572aa34b1c606de41b877b494e68a4cca36338d5
riccmar <riccardomarconi01@gmail.com>
2026-06-23 00:12:20 +0200

    Add division in subparts of notebook02 and add mps gpu

--- src/scripts/sae.py
+++ src/scripts/sae.py
@@ -7,9 +7,10 @@ import torch.nn as nn
 class SparseAutoencoder(nn.Module):
     def __init__(self, input_dim: int = 512, hidden_dim: int = 2048):
         super(SparseAutoencoder, self).__init__()
-        self.b_dec=nn.Parameter(torch.zeros(input_dim))
+        self.b_dec = nn.Parameter(torch.zeros(input_dim))
         self.encoder = nn.Linear(input_dim, hidden_dim)
         self.decoder = nn.Linear(hidden_dim, input_dim,bias=False)
+    
     def normalize_decoder_weights(self):
         with torch.no_grad():
             self.decoder.weight.data = F.normalize(self.decoder.weight.data, p=2, dim=0)
@@ -31,7 +32,7 @@ def sae_loss_function(x: torch.Tensor, x_hat: torch.Tensor, z: torch.Tensor, l1_
 def l2_normalize(embeddings: torch.Tensor) -> torch.Tensor:
     # F.normalize divide ogni vettore per la sua norma L2
     return F.normalize(embeddings, p=2, dim=1)
+
 if __name__=="__main__":
-    
     sae = SparseAutoencoder(input_dim=512, hidden_dim=2048)
     print(sae)
